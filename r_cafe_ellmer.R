f298173325e1831ecff254033658047e7fa00a1f
Ben Gerber <bgerber@uic.edu>
2025-01-09 20:47:09 -0500

    initial commit

---
+++ r_cafe_ellmer.R
@@ -0,0 +1,30 @@
+# install.packages("ellmer")
+library(ellmer)
+
+# download and run ollama
+# https://github.com/ollama/ollama
+
+chat <- chat_ollama(model = "llama3.2")
+chat$chat("Tell me three jokes about statisticians")
+
+question <- "
+  How can I compute the mean and median of variables a, b, c, and so on,
+  all the way up to z, grouped by age and sex.
+"
+
+chat <- chat_ollama(model = "llama3.2", system_prompt = "
+  You are an expert R programmer who prefers the tidyverse.
+  Just give me the code. I don't want any explanation or sample data.
+")
+
+chat$chat(question)
+
+result <- chat$extract_data(
+  "My name is Susan and I'm 13 years old",
+  type = type_object(
+    age = type_number(),
+    name = type_string()
+  )
+)
+
+result
